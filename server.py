10c8cb110ec985a63248875bd724a4c255cb1048
Игорь <89091819+ITech7750@users.noreply.github.com>
2023-11-11 13:39:37 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,43 +1,49 @@
+# Код сервера
 import socket
-import sys
+import struct
 
-# создаём сокет и связываем его с IP-адресом и портом
+def receive_file_size(sck: socket.socket):
+    # Эта функция обеспечивает получение байтов, 
+    # указывающих на размер отправляемого файла, 
+    # который кодируется клиентом с помощью 
+    # struct.pack(), функции, которая генерирует 
+    # последовательность байтов, представляющих размер файла.
+    fmt = "<Q"
+    expected_bytes = struct.calcsize(fmt)
+    received_bytes = 0
+    stream = bytes()
+    while received_bytes < expected_bytes:
+        tmp = sck.recv(expected_bytes - received_bytes)
+        stream += tmp
+        received_bytes += len(tmp)
+    filesize = struct.unpack(fmt, stream)[0]
+    return filesize
 
-sock = socket.socket()
-ip = "localhost"
-port = 9999
-sock.bind((ip, port))
 
-# сервер ожидает передачи информации
-sock.listen(10)
+def receive_file(sck: socket.socket, filename):
+    # Сначала считываем из сокета количество 
+    # байтов, которые будут получены из файла.
+    filesize = receive_file_size(sck)
+    # Открываем новый файл для сохранения
+    # полученных данных.
+    with open(filename, "wb") as f:
+        received_bytes = 0
+        # Получаем данные из файла блоками по
+        # 1024 байта до объема
+        # общего количество байт, сообщенных клиентом.
+        while received_bytes < filesize:
+            tmp = sck.recv(1024)
+            if tmp:
+                f.write(tmp)
+                received_bytes += len(tmp)
 
-while True:
-    # начинаем принимать соединения
-    conn, addr = sock.accept()
 
-    # выводим информацию о подключении
-    print('connected:', addr)
 
-    # получаем название файла
-    name_f = (conn.recv(1024)).decode ('UTF-8')
-
-    # открываем файл в режиме байтовой записи в отдельной папке 'sent'
-    f = open('sent/' + name_f,'wb')
-
-    while True:
-
-        # получаем байтовые строки
-        l = conn.recv(1024)
-
-        # пишем байтовые строки в файл на сервере
-        f.write(l)
-
-        if not l:
-            break
-
-    f.close()
-    conn.close()
-
-    print('File received')
-
-sock.close()
+with socket.create_server(("localhost", 8080)) as server:
+    print("Ожидание клиента...")
+    conn, address = server.accept()
+    print(f"{address[0]}:{address[1]} подключен.")
+    print("Получаем файл...")
+    receive_file(conn, "sent/image-received.png")
+    print("Файл получен.")
+print("Соединение закрыто.")
